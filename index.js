32b69ac96f430d9c1134f795be521767bce92152
RAPHAEL LEITE CAMPOS <raphaellc@users.noreply.github.com>
2021-09-29 01:35:25 -0300

    Limpeza do Texto

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const robots = {
     userInput: require('./robots/user-inputs.js'),
     text: require('./robots/text.js')
  } 
-function start(){
+async function start(){
  const content = {}
 
  robots.userInput(content)
- robots.text(content)
- console.log(content)
+ await robots.text(content)
 }
 start()
--- robots/text.js
+++ robots/text.js
@@ -1,16 +1,36 @@
 const algorithmia = require('algorithmia')
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apikey
-function robot(content){
+async function robot(content){
     console.log(`Recebi com sucesso o content: ${content.searchTerm}`)
-    fetchContentFromWikipedia(content)
-    //sanitizeContent(content)
+     await fetchContentFromWikipedia(content)
+    sanitizeContent(content)
     //breakContentIntoSentences(content)
    async function fetchContentFromWikipedia(content){
         const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
         const wikipediaAlgorithm = algorithmiaAuthenticated.algo("web/WikipediaParser/0.1.2")
         const wikipediaResponde = await wikipediaAlgorithm.pipe(content.searchTerm)
         const wikipediaContent = wikipediaResponde.get()
-        console.log(wikipediaContent)
+        content.sourceContentOriginal = wikipediaContent.content
+    }
+    function sanitizeContent(content){
+        const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
+        const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
+
+        console.log(withoutDatesInParentheses)
+
+        function removeBlankLinesAndMarkdown(text){
+            const allLines = text.split('\n')
+            const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+                if (line.trim().length === 0 || line.trim().startsWith('=')){
+                    return false
+                }
+                return true
+            })
+            return withoutBlankLinesAndMarkdown.join(' ')          
+        }
+        function removeDatesInParentheses(text) {
+            return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
+        }
     }
 }
 module.exports = robot
